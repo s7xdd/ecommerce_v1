3fd04c43f212211a9924121df7487a4ef98af73b
Mohammed Shamil <kmshamil123@gmail.com>
2024-11-01 17:45:19 +0530

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "tailwindui.com",
+      "images.unsplash.com",
+      "i.ibb.co",
+      "i.postimg.cc",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
 
 export default nextConfig;
