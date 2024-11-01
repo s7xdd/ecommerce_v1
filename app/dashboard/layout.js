94a98959baef7db9b1edaa6f722d8683b8e618f2
Mohammed Shamil <kmshamil123@gmail.com>
2024-11-02 01:30:47 +0530

    Fixed admin Header

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="w-full flex items-center justify-center flex-col">
+    <div className="w-full flex items-center justify-center flex-col mt-20">
       <Header />
       {children}
     </div>
