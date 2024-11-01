cf4288cba11a0ada52d66c252c9df8281ab81585
Mohammed Shamil <kmshamil123@gmail.com>
2024-11-01 17:22:26 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('/hero-pattern.png')",
+      }),
+      backgroundPosition: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
